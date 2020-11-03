ee2d5b671b96b33ab3769ba0f3f8212338bec951
Aseem <aseemanand2@gmail.com>
2020-11-03 21:39:56 +0530

    [Aseem] Added created repo for Ado.Net

---
+++ Ado.NetEmployeePayroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ado.NetEmployeePayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
